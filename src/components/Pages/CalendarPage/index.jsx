4c4d7acc3c9f05f897e81807b9fd926e5d3a744c
Gabriel Moraes <moraes.madm@gmail.com>
2023-03-14 19:10:25 -0300

    Adds calendar

--- src/components/Pages/CalendarPage/index.jsx
+++ src/components/Pages/CalendarPage/index.jsx
@@ -1,7 +1,18 @@
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import moment from 'moment'
+
 export default function CalendarPage() {
+  const localizer = momentLocalizer(moment)
+
   return (
     <div>
-      here goes calendar
+      <Calendar
+        events={[]}
+        localizer={localizer}
+        style={{ height: 500 }}
+      />
     </div>
   )
 }
