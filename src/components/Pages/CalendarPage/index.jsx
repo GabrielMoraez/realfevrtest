f7ad2bf59d409edd63647c1fdc6467d257034114
Gabriel Moraes <moraes.madm@gmail.com>
2023-03-14 19:23:39 -0300

    Creates a emojiDecoder and memoize it

--- src/components/Pages/CalendarPage/index.jsx
+++ src/components/Pages/CalendarPage/index.jsx
@@ -1,6 +1,5 @@
 import { Calendar, momentLocalizer } from 'react-big-calendar'
-import "react-big-calendar/lib/css/react-big-calendar.css";
-
+import "react-big-calendar/lib/css/react-big-calendar.css"
 import moment from 'moment'
 
 export default function CalendarPage() {
--- src/components/Pages/EmojiPage/index.jsx
+++ src/components/Pages/EmojiPage/index.jsx
@@ -1,7 +1,25 @@
+import { useParams } from 'react-router-dom'
+import emojiList from 'emojis-list'
+import { useMemo } from 'react'
+
 export default function EmojiPage() {
+  const { hash } = useParams()
+
+  const decodedEmojis = useMemo(() => {
+    const decodedEmojis = []
+    
+    for (let i = 0; i < hash.length; i += 2) {
+      const hex = hash.slice(i, i + 1)
+      const index = parseInt(hex, 36) % emojiList.length
+      decodedEmojis.push(emojiList[index])
+    }
+
+    return decodedEmojis
+  }, [hash])
+
   return (
     <div>
-      here goes emojis
+      {decodedEmojis}
     </div>
   )
 }
