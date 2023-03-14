da96f205f1764fc0f0c61ebc13791a2053c9278a
Gabriel Moraes <moraes.madm@gmail.com>
2023-03-14 19:30:35 -0300

    Adds calendar to emoji page

--- src/components/Pages/EmojiPage/index.jsx
+++ src/components/Pages/EmojiPage/index.jsx
@@ -1,9 +1,12 @@
 import { useParams } from 'react-router-dom'
 import emojiList from 'emojis-list'
 import { useMemo } from 'react'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
 
 export default function EmojiPage() {
   const { hash } = useParams()
+  const localizer = momentLocalizer(moment)
 
   const decodedEmojis = useMemo(() => {
     const decodedEmojis = []
@@ -17,9 +20,33 @@ export default function EmojiPage() {
     return decodedEmojis
   }, [hash])
 
+  const handleSelectSlot = (slotInfo) => {
+    console.log(slotInfo)
+  };
+
   return (
     <div>
-      {decodedEmojis}
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          textAlign: 'center',
+          fontSize: 20,
+          marginBottom: 50
+        }}
+      >
+        YOUR EMOJIS: {decodedEmojis}
+        <br />
+        YOU CAN ADD THEM TO A DATE BELOW
+      </div>
+      <Calendar
+        localizer={localizer}
+        selectable
+        events={[]}
+        style={{ height: 500 }}
+        onSelectSlot={handleSelectSlot}
+      />
+
     </div>
   )
 }
